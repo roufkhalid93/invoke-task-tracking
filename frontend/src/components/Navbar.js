b620e5a3f8c1630eb1ffaa4c730aca019f7ffb90
roufkhalid93 <roufkhalid93@gmail.com>
2025-01-25 15:08:46 +0800

    Add frontend files

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <header>
+            <div className='container'>
+                <Link to="/">
+                    <h1>TT (Task Tracker)</h1>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar

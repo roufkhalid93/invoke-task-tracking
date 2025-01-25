d8b870c5e95417221a66e35bd4f7ee57655e37e0
roufkhalid93 <roufkhalid93@gmail.com>
2025-01-26 01:43:31 +0800

    Add error handling response

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -29,6 +29,15 @@ const getTask = async (req, res) => {
 const createTask = async (req, res) => {
     const {title, description} = req.body
 
+    let emptyFields = []
+
+    if(!title) {
+        emptyFields.push('title')
+    }
+    if(emptyFields.length > 0) {
+        return res.status(400).json({ error: 'Please fill in all the fields', emptyFields})
+    }
+
     //add doc to db
     try{
         const task = await Task.create({title, description})

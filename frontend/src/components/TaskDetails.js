97e634296f60c062bedd9698bed43ac769ac7842
roufkhalid93 <roufkhalid93@gmail.com>
2025-01-26 01:37:21 +0800

    Add delete task feature and error handling response

--- frontend/src/components/TaskDetails.js
+++ frontend/src/components/TaskDetails.js
@@ -1,9 +1,28 @@
+import { useTasksContext } from "../hooks/useTasksContext"
+
+//date formatting fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 const TaskDetails = ({ task }) => {
+    const { dispatch } = useTasksContext()
+
+    const handleClick = async () => {
+        const response = await fetch ('/api/tasks/' + task._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if(response.ok) {
+            dispatch({type: 'DELETE_TASK', payload: json})
+        }
+    }
+
     return (
         <div className="task-details">
             <h4>{task.title}</h4>
             <p><strong>Description:</strong> {task.description}</p>
-            <p>{task.createdAt}</p>
+            <p>{formatDistanceToNow(new Date(task.createdAt), { addSuffix: true })}</p>
+            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
         </div>
     )
 }
